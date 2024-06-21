30168d378e9b95aaf54000d8ba238b2888512fad
Alexander Wilms <f.alexander.wilms@gmail.com>
2024-06-21 17:43:27 +0200

    anitya: Query project name

--- release_monitoring_api.py
+++ release_monitoring_api.py
@@ -1,14 +1,17 @@
 import json
-from pprint import pprint
 
 import requests
 
-# https://release-monitoring.org/static/docs/api.html#get--api-v2-versions-
-URL = "https://release-monitoring.org/api/v2/versions/"
-
 # https://release-monitoring.org/project/201/
-response = requests.get(url=URL, params={"project_id": 201})
 
+# https://release-monitoring.org/static/docs/api.html#get--api-project-(int-project_id)
+response = requests.get(url="https://release-monitoring.org/api/project/201")
 json_dict = json.loads(response.content)
-pprint(json_dict.keys())
+# pprint(json_dict.keys())
+print(json_dict["name"])
+
+# https://release-monitoring.org/static/docs/api.html#get--api-v2-versions-
+response = requests.get(url="https://release-monitoring.org/api/v2/versions/", params={"project_id": 201})
+json_dict = json.loads(response.content)
+# pprint(json_dict.keys())
 print(json_dict["latest_version"])
